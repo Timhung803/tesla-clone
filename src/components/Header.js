acb3ddee8fc5fb1a4bde4d96a48fe7b01b022d76
Timhung803 <timhkt@gmail.com>
2022-07-02 03:48:39 +1200

    Used useState and added transition to Sidebar component, set up Redux removed boilerplate code from project, used Redux to create carslice & mapped carSlice in the Main Menu and Sidebar component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,29 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import MenuIcon from "@mui/icons-material/Menu";
 import CloseIcon from "@mui/icons-material/Close";
-
+import { selectCars } from "../features/car/carSlice";
+import { useSelector } from 'react-redux';
 function Header() {
+  const [burgerStatus, setBurgerStatus] = useState(false);
+  const cars = useSelector(selectCars);
+  console.log(cars);
   return (
     <Container>
       <a>
         <img src="/images/logo.svg" alt="Logo" />
       </a>
       <Menu>
-        <a href="#">Model S</a>
-        <a href="#">Model 3</a>
-        <a href="#">Model X</a>
-        <a href="#">Model Y</a>
+        {cars && cars.map((car, index)=>(
+          <a key={index} href="#">{car}</a>
+        ))}
       </Menu>
       <RightMenu>
         <a href="#">Shop</a>
         <a href="#">Tesla Account</a>
-        <CustomMenu />
+        <CustomMenu onClick={() => setBurgerStatus(true)}/>
       </RightMenu>
-      <BurgerNav>
+      <BurgerNav show={burgerStatus}>
         <CloseWrapper>
-          <CustomClose />
+          <CustomClose onClick={() => setBurgerStatus(false)}/>
         </CloseWrapper>
+        {cars && cars.map((car, index)=>(
+          <li key={index}><a href="*">{car}</a></li>
+        ))}
+        
         <li>
           <a href="*">Existing Inventory</a>
         </li>
@@ -115,6 +122,8 @@ const BurgerNav = styled.div`
   display: flex;
   flex-direction: column;
   text-align: start;
+  transform: ${props => props.show ? 'translateX(0)': 'translateX(100%)'};
+  transition: transform 0.6s;
   li {
     padding: 15px 0;
     border-bottom: 1px solid rgba(0, 0, 0, 0.2);
